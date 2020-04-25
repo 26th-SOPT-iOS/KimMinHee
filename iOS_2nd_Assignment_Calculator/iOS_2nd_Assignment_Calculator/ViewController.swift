321b9734c12e19a9bcb3fc0bcfb2d908c795d7ab
xwoud <xwoud@naver.com>
2020-04-26 03:18:13 +0900

    README 폴더

---
+++ iOS_2nd_Assignment_Calculator/iOS_2nd_Assignment_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_2nd_Assignment_Calculator
+//
+//  Created by 김민희 on 26/04/2020.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_2nd_Seminar/iOS_2nd_Seminar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_2nd_Seminar
+//
+//  Created by 김민희 on 25/04/2020.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
