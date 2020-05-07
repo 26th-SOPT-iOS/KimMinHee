6523557679da45c4af43eee50c6ad285bd39fa05
xwoud <xwoud@naver.com>
2020-05-08 00:48:31 +0900

    2차 수정

--- IOS_assignment_login/IOS_assignment_login/MainViewController.swift
+++ IOS_assignment_login/IOS_assignment_login/MainViewController.swift
@@ -9,28 +9,47 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
 
     override func viewDidLoad() {
-        super.viewDidLoad()
- 
         
+        super.viewDidLoad()
+        //navigationController?.isNavigationBarHidden = true
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        // navigation tap 투명하게
+
+        idTextField.layer.cornerRadius = 22
+        pwTextField.layer.cornerRadius = 22
+        loginButton.layer.cornerRadius = 24
+
+        idTextField.addLeftPadding()
+        pwTextField.addLeftPadding()
+
         // Do any additional setup after loading the view.
     }
     
-
+    
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
 
+    @IBOutlet weak var loginButton: UIButton!
     @IBAction func GoLogin(_ sender: Any) {
         // 로그인 버튼
-        guard let receiveViewController = self.storyboard?.instantiateViewController(identifier: "loginViewController") as? LoginViewController else {return}
-        
+        guard let receiveViewController = self.storyboard?.instantiateViewController(identifier: "scrollViewController") as? ScrollViewController else {return}
+        /*
         receiveViewController.yourId = idTextField.text
         receiveViewController.yourPw = pwTextField.text
+ */
+        receiveViewController.modalPresentationStyle = .fullScreen
         self.present(receiveViewController, animated: true, completion: nil)
         idTextField.text=""
         pwTextField.text=""
     }
+    
+    @IBOutlet weak var SignText: UILabel!
     
     @IBAction func SignViewMove(_ sender: Any) {
         // 회원가입 버튼
@@ -38,5 +57,13 @@ class MainViewController: UIViewController {
         
         self.navigationController?.pushViewController(SignPush, animated: true)
         
+    }
+    
+}
+extension UITextField {
+    func addLeftPadding() {
+      let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 17, height: self.frame.height))
+      self.leftView = paddingView
+      self.leftViewMode = ViewMode.always
     }
 }
---
+++ IOS_assignment_login/IOS_assignment_login/ScrollViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ScrollViewController.swift
+//  IOS_assignment_login
+//
+//  Created by 김민희 on 01/05/2020.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        self.scroll.delegate = self
+        self.scroll.contentInsetAdjustmentBehavior = .never
+        super.viewDidLoad()
+    }
+
+    @IBOutlet weak var scroll: UIScrollView!
+    @IBOutlet weak var image: UIImageView!
+
+    @IBOutlet weak var imageHeight: NSLayoutConstraint!
+    //let originHeaderImageHeight = imageHeight
+}
+   
+extension ScrollViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yPosition = scrollView.contentOffset.y
+        if yPosition < 0 {
+
+            let scale  = 1 + ((-yPosition) * 2 / image.frame.height)
+            image.transform = CGAffineTransform(scaleX: scale, y: scale)
+            var imageViewFrame = image.frame
+            imageViewFrame.origin.y = yPosition
+            image.frame = imageViewFrame
+            
+        } else {
+            
+        }
+    }
+    
+}
+
+
