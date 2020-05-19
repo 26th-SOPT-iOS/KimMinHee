f8a32ed3a90a52b193905a00461a20ab04c1fa1d
xwoud <xwoud@naver.com>
2020-05-20 00:34:34 +0900

    회원가입 data Decoding

--- 과제/iOS_assignment_login/iOS_assignment_login/Resources/API/SignupData.swift
+++ 과제/iOS_assignment_login/iOS_assignment_login/Resources/API/SignupData.swift
@@ -5,7 +5,6 @@
 //  Created by 김민희 on 16/05/2020.
 //  Copyright © 2020 김민희. All rights reserved.
 //
-/*
 import Foundation
 struct SignupData: Codable {
     
@@ -17,19 +16,12 @@ struct SignupData: Codable {
         case status = "status"
         case success = "success"
         case message = "message"
-        case data = "data"
     }
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         status = (try? values.decode(Int.self, forKey: .status)) ?? -1
-        success = (try? values.decode(Bool.self, forKey: .success)) ?? true
+        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
         message = (try? values.decode(String.self, forKey: .message)) ?? ""
-        data = (try? values.decode(TokenData.self, forKey: .data)) ?? nil
     }
 }
-
-struct TokenData: Codable{
-    var jwt: String
-}
-*/
